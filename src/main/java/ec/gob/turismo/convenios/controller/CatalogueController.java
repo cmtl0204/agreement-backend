6088e36cf7f8f26e284bd60c251dc4ab58ea966f
cmtl0204 <cesar.tamayo0204@gmail.com>
2024-08-06 08:09:38 -0500

    fix bugs

--- src/main/java/ec/gob/turismo/convenios/controller/CatalogueController.java
+++ src/main/java/ec/gob/turismo/convenios/controller/CatalogueController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,7 @@ public class CatalogueController {
     @Qualifier("defaultMapper")
     private final ModelMapper mapper;
 
+    @CrossOrigin(origins = "*")
     @GetMapping
     public ResponseEntity<List<CatalogueDTO>> findAll() {
         List<CatalogueDTO> list = service.findCatalogue().stream().map(this::convertToDto).toList();
